59312bcf81cf409d2b2e13bdbea640aab36af0fc
Paul <paulrdkirby@gmail.com>
2016-02-03 01:09:04 -0700

    Changed to Evolve theme, basic styling and set up for home page. Custom CSS for Main menu

--- wp-config.php
+++ wp-config.php
@@ -21,7 +21,7 @@
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('WP_CACHE', true); //Added by WP-Cache Manager
-define( 'WPCACHEHOME', '/mnt/storage/Paul/Developer/Web/AbsolutePressure/wp-content/plugins/wp-super-cache/' ); //Added by WP-Cache Manager
+define( 'WPCACHEHOME', '/Users/Paul/Developer/Web/AbsolutePressure/wp-content/plugins/wp-super-cache/' ); //Added by WP-Cache Manager
 define('DB_NAME', 'absolute_pressure');
 
 /** MySQL database username */
